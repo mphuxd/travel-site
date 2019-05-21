f92717858409af8967ad6b220120bde577d717e1
Matthew <matthewpham2012@gmail.com>
2019-05-20 17:50:27 -0700

    Added legacy browser support with modernizr for svg/png and flexbox

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,11 +1,24 @@
 var gulp = require('gulp'), 
 svgSprite = require('gulp-svg-sprite'),
 rename = require('gulp-rename'),
-del = require('del');
+del = require('del'),
+svg2png = require('gulp-svg2png');
 
 var config = {
+	shape: {
+		spacing: {
+			padding: 1
+		}
+	},
 	mode: {
 		css: {
+			variables: {
+				replaceSvgWithPng: function() {
+					return function(sprite, render) {
+						return render(sprite).split('.svg').join('.png');
+					}
+				}
+			},
 			sprite: 'sprite.svg',
 			render: {
 				css: {
@@ -20,14 +33,20 @@ gulp.task('beginClean', function(){
 	return del(['./app/temp/sprite', './app/assets/images/sprites']);
 });
 
-gulp.task('createSprite',['beginClean'] , function() {
+gulp.task('createSprite', ['beginClean'] , function() {
 	return gulp.src('./app/assets/images/icons/**/*.svg')
 		.pipe(svgSprite(config))
 		.pipe(gulp.dest('./app/temp/sprite/'));
 });
 
-gulp.task('copySpritGraphic', ['createSprite'], function(){ 
-	return gulp.src('./app/temp/sprite/css/**/*.svg')
+gulp.task('createPngCopy', ['createSprite'] , function() {
+	return gulp.src('./app/temp/sprite/css/*.svg')
+	.pipe(svg2png())
+	.pipe(gulp.dest('./app/temp/sprite/css'));
+});
+
+gulp.task('copySpritGraphic', ['createPngCopy'], function() { 
+	return gulp.src('./app/temp/sprite/css/**/*.{svg,png}')
 	.pipe(gulp.dest('./app/assets/images/sprites'));
 });
 
@@ -41,4 +60,4 @@ gulp.task('endClean',['copySpritGraphic', 'copySpriteCSS'] , function(){
 	return del('./app/temp/sprite');
 });
 
-gulp.task('icons', ['beginClean' ,'createSprite', 'copySpritGraphic', 'copySpriteCSS', 'endClean']);
+gulp.task('icons', ['beginClean' ,'createSprite', 'createPngCopy', 'copySpritGraphic', 'copySpriteCSS', 'endClean']);
